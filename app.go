ac7a88c87414029b26c385fd21f96b78fe7448a7
Ashwini Patankar <ashwinispatankar@gmail.com>
2017-10-05 14:01:18 +0000

    Added templating

--- app.go
+++ app.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+  "html/template"
   "log"
   "net/http"
+  "path/filepath"
 )
 
 func main() {
   fs := http.FileServer(http.Dir("static"))
-  http.Handle("/", fs)
+  http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+  http.HandleFunc("/", serveTemplate)
 
   log.Println("Listening...")
   http.ListenAndServe(":3000", nil)
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+  lp := filepath.Join("templates", "layout.html")
+  fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+  tmpl, _ := template.ParseFiles(lp, fp)
+  tmpl.ExecuteTemplate(w, "layout", nil)
 }
