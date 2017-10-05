ce461587d396dd3482f8ebef8d5c3e6218932007
Ashwini Patankar <ashwinispatankar@gmail.com>
2017-10-05 14:05:08 +0000

    Added 404 and README

--- app.go
+++ app.go
@@ -1,26 +1,52 @@
 package main
 
 import (
-  "html/template"
-  "log"
-  "net/http"
-  "path/filepath"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-  fs := http.FileServer(http.Dir("static"))
-  http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", serveTemplate)
 
-  http.HandleFunc("/", serveTemplate)
-
-  log.Println("Listening...")
-  http.ListenAndServe(":3000", nil)
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", nil)
 }
 
 func serveTemplate(w http.ResponseWriter, r *http.Request) {
-  lp := filepath.Join("templates", "layout.html")
-  fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+	lp := filepath.Join("templates", "layout.html")
+	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
 
-  tmpl, _ := template.ParseFiles(lp, fp)
-  tmpl.ExecuteTemplate(w, "layout", nil)
+	// Return a 404 if the template doesn't exist
+	info, err := os.Stat(fp)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.NotFound(w, r)
+			return
+		}
+	}
+
+	// Return a 404 if the request is for a directory
+	if info.IsDir() {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		// Log the detailed error
+		log.Println(err.Error())
+		// Return a generic "Internal Server Error" message
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	if err := tmpl.ExecuteTemplate(w, "layout", nil); err != nil {
+		log.Println(err.Error())
+		http.Error(w, http.StatusText(500), 500)
+	}
 }
